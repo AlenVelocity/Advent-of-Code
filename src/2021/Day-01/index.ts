d68abe0b7f241d09a5be69c82c9be1782f83451c
alensaito1 <erwinsmithscout1@gmail.com>
2021-12-04 14:17:51 +0530

    feat): Solution - Day 01

---
+++ src/2021/Day-01/index.ts
@@ -0,0 +1,24 @@
+type Result = Record<'partone' | 'parttwo', number>
+/**
+ * @see [Day 01](https://adventofcode.com/2021/day/1)
+ * @param lines - Input array of lines
+ * @returns {Result}
+ */
+export const run = (lines: Array<number>): Result => {
+    const increases = (data: Array<number>, interval: number) =>
+        data.reduce((total, currVal, currIndex) => (data[currIndex + interval] > currVal ? total + 1 : total), 0)
+
+    return {
+        partone: increases(lines, 1),
+        parttwo: increases(lines, 2)
+    }
+}
+
+export const parse = (input: string): Array<number> => {
+    return input
+        .trim()
+        .split('\n')
+        .map((x) => {
+            return parseInt(x, 10)
+        })
+}
